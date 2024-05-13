08e887702dd904a19a6e409576617801f73b75f2
Jinyang-park <dbsskdud60@gmail.com>
2024-05-14 01:28:12 +0900

    feat :mysqlport 주석처리

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.listen(process.env.PORT || 3000, () => {
 // MYSQL
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
+  // port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
