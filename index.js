89102bb0d4f8919a094698cce4c4d2de66ebf16f
Jinyang-park <dbsskdud60@gmail.com>
2024-05-23 17:19:18 +0900

    s3추가

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import mysql from 'mysql';
 import cookieParser from 'cookie-parser';
 import main from './src/router/main.js';
 import cart from './src/router/cart.js';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import { S3Client } from '@aws-sdk/client-s3';
 
 dotenv.config();
 
@@ -24,6 +27,26 @@ app.use(
 );
 app.use(express.json({ extended: true }));
 
+//S3
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+  region: 'ap-northeast-2',
+});
+
+// 이미지 파일 업로드
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_S3_BUCKET,
+    key: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
+
 // MYSQL
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
@@ -41,6 +64,10 @@ app.use('/cart', cart);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/upload', upload.array('photos'), (req, res) => {
+  res.send(req.files);
 });
 
 // GET /health 요청에 대해 상태코드 200으로 응답하는 API
